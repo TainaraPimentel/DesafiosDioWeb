06d47baa49afd743b2d5da786eecd1c054923392
TainaraMoraisPimentel <tainara_pimentel@outlook.com.br>
2024-01-21 13:03:49 -0300

    create pastes

---
+++ Bootcamp_prog_zero/Desafio_estrutura_dados_objetos/EscrevendoClassesJogo.js
@@ -0,0 +1,22 @@
+class Heroi {
+	constructor(name, idade, tipo, ataque) {
+		this.name = name
+		this.idade = idade
+		this.tipo = tipo
+		this.ataque = ataque
+	}
+
+	atacar(){
+		console.log(`O ${tipo} atacou usando ${ataque}`)
+	}
+}
+
+let heroi1 = new Heroi ("Thiago", 28, "mago", "magia")
+let heroi2 = new Heroi ("Junior", 30, "guerreiro", "espada")
+let heroi3 = new Heroi ("Rafael", 20, "monge", "artes marciais")
+let heroi4 = new Heroi ("Carlos", 24, "ninja", "shuriken")
+
+heroi1.atacar()
+heroi2.atacar()
+heroi3.atacar()
+heroi4.atacar()
